3bc2652d0e7392168a1c829e27485d1dd20a44d2
Wu Jian Ping <wujjpp@hotmail.com>
2022-02-09 18:21:26 +0800

    .

--- tcp-server.js
+++ tcp-server.js
@@ -1,21 +1,21 @@
-const net = require('net')
+const net = require("net");
 
-const LOCAL_PORT = 16379
+const LOCAL_PORT = 16379;
 
 const server = net.createServer((socket) => {
-  socket.on('data', (data) => {
-    console.log(data.length, data.toString('utf-8'))    
-  })
-})
+    socket.on("data", (data) => {
+        console.log(data.length, data.toString("utf-8"));
+    });
+});
 
-server.on('error', (err) => {
-  console.log(err)
-})
+server.on("error", (err) => {
+    console.log(err);
+});
 
 server.listen(LOCAL_PORT, (err) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log(`server listen on ${LOCAL_PORT}`)
-  }
-})
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`server listen on ${LOCAL_PORT}`);
+    }
+});
